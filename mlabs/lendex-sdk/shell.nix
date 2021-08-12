7a997a409d68cee3348235d0e07257552b019a0e
zygomeb <zygomeb@gmail.com>
2021-08-12 18:06:00 +0200

    copying stuff

--- mlabs/lendex-sdk/shell.nix
+++ mlabs/lendex-sdk/shell.nix
@@ -1,11 +1,14 @@
- pkgs ? import <nixpkgs> {} }:
 let
   sources = import ./nix/sources.nix {};
+  pkgs = import sources.nixpkgs {};
+  pkgs_master = import sources.nixpkgs_master {};
   easy-ps = import sources.easy-purescript-nix {};
 in
+
 pkgs.mkShell {
   buildInputs = builtins.attrValues {
     inherit (pkgs) gnumake nodejs;
-    inherit (easy-ps) purs pulp purp psc-package dhall-simple spago; # psa pscid spago2nix purty zephyr;
+    inherit (pkgs_master.nodePackages) purescript-language-server;
+    inherit (easy-ps) purs pulp purp psc-package dhall-simple spago psa pscid spago2nix zephyr;
   };
 }
