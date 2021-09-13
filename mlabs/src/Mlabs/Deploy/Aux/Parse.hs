eaeaa218670ef9dd0b2c9d2c5c5c2549b3810211
cstml <cstmlcodes@gmail.com>
2021-09-13 16:24:16 +0100

    update: working Parse

--- mlabs/src/Mlabs/Deploy/Aux/Parse.hs
+++ mlabs/src/Mlabs/Deploy/Aux/Parse.hs
@@ -1,4 +1,7 @@
-module Mlabs.Deploy.Aux.Parse where
+{-#LANGUAGE UndecidableInstances#-}
+module Mlabs.Deploy.Aux.Parse (getEUTXo) 
+
+where
 
 import System.IO
 import Prelude as Hask
@@ -10,16 +13,56 @@ import Control.Lens
 import Data.Map (Map)
 import GHC.Generics ( Generic )
 import qualified Data.HashMap.Lazy as Hm
+import qualified Data.Scientific as Scientific
 
-parse :: String -> Maybe Object
-parse str = decode . packChars $ str   
+data Transaction = Transaction 
+  { t'id :: Hask.String
+  , t'ix :: Hask.Int
+  , t'llace :: Hask.Float 
+  , t'tokens   :: [Tokens]
+  }
+  deriving Hask.Show
 
-getEUTXo :: IO [String]
-getEUTXo = do 
-    file <- readFile "deploy/test.json"
-    let t = parse file
-    putStrLn file
-    let contents = t ^.. each
-    print contents
-    let hm = show <$> (maybe mempty Hm.toList $ t) ^.. folded . _1 
-    return hm
+data Value = Value 
+  { v'lovelace :: Hask.Float
+  , v'tokens   :: [Tokens]
+  }  
+  deriving Hask.Show
+
+data Tokens = Tokens
+  { t'hash :: Hask.String
+  , t'name :: Hask.String
+  , t'amm  :: Hask.Float
+  }
+  deriving Hask.Show
+
+getEUTXo :: FilePath -> IO [Transaction]
+getEUTXo fileP = do 
+    file <- readFile fileP
+    let t         = parse file
+        hm        = (getIx . read . show) <$> (maybe mempty Hm.toList $ t) ^.. folded . _1
+        lovelace  =  t ^.. folded . folded . key "value"  . key "lovelace" . _Number 
+        lovelace' = Scientific.toRealFloat <$> lovelace
+        tokens    = (filter (\x -> fst x /= "lovelace") . Hm.toList) <$> (t ^.. folded . folded . key "value" . _Object)  
+        tokens'   =  fmap (unpack . fst) <$> tokens 
+        tokens''  = aux <$> tokens
+        tokens''' = aux' <$> tokens
+        tokensf   = zipWith3 (zipWith3 (\a b c ->  (a, b, c) )) tokens' tokens'' tokens'''
+        tokensf'  = fmap (\(a,b,c) -> Tokens a b c ) <$> tokensf
+    return $ (Hask.uncurry. Hask.uncurry . Hask.uncurry) Transaction <$> zip (zip hm lovelace') tokensf'
+    where
+        getIx :: String -> (String,Int)
+        getIx  =  n . reverse 
+            where 
+                n (x:'#':xs) = (reverse xs,(read [x]))
+
+        aux x  = x ^..  folded . folded . _Object . to Hm.toList . folded . to fst . to unpack
+        aux' x = x ^..  folded . folded . _Object . to Hm.toList . folded . to snd . _Number . to Scientific.toRealFloat 
+
+        unpack :: Show a =>  a -> String
+        unpack = read . show
+
+        parse :: String -> Maybe Object
+        parse str = decode . packChars $ str   
+
+testGetEUTXo = getEUTXo "./deploy/test3.json"
