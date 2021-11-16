068be26f7e9cfd5e602d5fb49c5d6f7cb7a51f84
Rory Tyler Hayford <rory.hayford@protonmail.com>
2021-11-16 18:54:22 +0700

    Let WBE balance payment contract

--- mlabs/src/Mlabs/IntegrationTest/PabWbe/Contracts/Payment.hs
+++ mlabs/src/Mlabs/IntegrationTest/PabWbe/Contracts/Payment.hs
@@ -7,22 +7,15 @@ module Mlabs.IntegrationTest.PabWbe.Contracts.Payment (
 import Control.Monad (void)
 
 import Data.Aeson (FromJSON, ToJSON)
-import Data.Map qualified as Map
 import Data.OpenApi.Schema qualified as Schema
 import Data.Text (Text)
 import Data.Void (Void)
 
 import GHC.Generics (Generic)
 
-import Mlabs.IntegrationTest.Utils
-
 import Ledger (
-  ChainIndexTxOut (..),
   PubKeyHash,
-  TxId,
-  TxOutRef (TxOutRef),
   getCardanoTxId,
-  pubKeyHashAddress,
  )
 import Ledger.Ada qualified as Ada
 import Ledger.Constraints qualified as Constraints
@@ -41,9 +34,8 @@ import PlutusTx.Prelude
 
 import Prelude qualified as Hask
 
-data Payment = Payment
-  { fromPkh :: PubKeyHash
-  , toPkh :: PubKeyHash
+newtype Payment = Payment
+  { toPkh :: PubKeyHash
   }
   deriving stock (Hask.Eq, Hask.Ord, Hask.Show, Generic)
   deriving anyclass (FromJSON, ToJSON, Schema.ToSchema, Playground.ToSchema)
@@ -52,32 +44,11 @@ type PaymentSchema = Endpoint "payment" Payment
 
 payFromTo :: Payment -> Contract () PaymentSchema Text ()
 payFromTo Payment {..} = do
-  let constraints =
-        mconcat
-          [ Constraints.mustSpendPubKeyOutput oref
-          , Constraints.mustPayToPubKey toPkh $ Ada.adaValueOf 5
-          ]
-
-      lookups =
-        Constraints.unspentOutputs $
-          Map.singleton oref txOut
-
-      oref = TxOutRef refId 2
-
-      txOut =
-        PublicKeyChainIndexTxOut
-          (pubKeyHashAddress fromPkh)
-          (Ada.adaValueOf 10)
-
   txId <-
     getCardanoTxId
-      <$> submitTxConstraintsWith @Void lookups constraints
+      <$> submitTxConstraintsWith @Void Hask.mempty constraint
   logInfo @Hask.String $ "Awaiting tx completion: " <> Hask.show txId
   void $ awaitTxConfirmed txId
   logInfo @Hask.String "Tx confirmed, payment complete"
   where
-    refId :: TxId
-    refId =
-      unsafeDecode
-        "TxId"
-        "{\"getTxId\" : \"206e4a7e1b8a8004d41546ae28089cc4c00853d4f285c45ca62ba8a81b271f41\"}"
+    constraint = Constraints.mustPayToPubKey toPkh $ Ada.adaValueOf 5
