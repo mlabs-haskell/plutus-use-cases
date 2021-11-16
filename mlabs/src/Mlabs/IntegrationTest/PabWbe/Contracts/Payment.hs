c8848a8f64f50d32cfe0b9ab0dcccb1010f7e8a0
Rory Tyler Hayford <rory.hayford@protonmail.com>
2021-11-16 18:02:32 +0700

    PHK-to-PHK payment

--- mlabs/src/Mlabs/IntegrationTest/PabWbe/Contracts/Payment.hs
+++ mlabs/src/Mlabs/IntegrationTest/PabWbe/Contracts/Payment.hs
@@ -8,32 +8,36 @@ import Control.Monad (void)
 
 import Data.Aeson (FromJSON, ToJSON)
 import Data.Map qualified as Map
+import Data.OpenApi.Schema qualified as Schema
 import Data.Text (Text)
 import Data.Void (Void)
 
 import GHC.Generics (Generic)
 
+import Mlabs.IntegrationTest.Utils
+
 import Ledger (
   ChainIndexTxOut (..),
   PubKeyHash,
+  TxId,
+  TxOutRef (TxOutRef),
   getCardanoTxId,
   pubKeyHashAddress,
  )
 import Ledger.Ada qualified as Ada
 import Ledger.Constraints qualified as Constraints
 
+import Playground.Contract qualified as Playground
+
 import Plutus.Contract (
+  Contract,
   Endpoint,
-  Promise,
   awaitTxConfirmed,
-  endpoint,
   logInfo,
   submitTxConstraintsWith,
  )
 
 import PlutusTx.Prelude
-
-import Schema (ToSchema)
 
 import Prelude qualified as Hask
 
@@ -41,31 +45,39 @@ data Payment = Payment
   { fromPkh :: PubKeyHash
   , toPkh :: PubKeyHash
   }
-  deriving stock (Generic)
-  deriving anyclass (FromJSON, ToJSON, ToSchema)
+  deriving stock (Hask.Eq, Hask.Ord, Hask.Show, Generic)
+  deriving anyclass (FromJSON, ToJSON, Schema.ToSchema, Playground.ToSchema)
 
 type PaymentSchema = Endpoint "payment" Payment
 
-payFromTo :: Promise () PaymentSchema Text ()
-payFromTo = endpoint @"payment" $ \Payment {..} -> do
-  let cs =
+payFromTo :: Payment -> Contract () PaymentSchema Text ()
+payFromTo Payment {..} = do
+  let constraints =
         mconcat
           [ Constraints.mustSpendPubKeyOutput oref
           , Constraints.mustPayToPubKey toPkh $ Ada.adaValueOf 5
           ]
 
-      ls = Constraints.unspentOutputs utxos
+      lookups =
+        Constraints.unspentOutputs $
+          Map.singleton oref txOut
 
-      utxos = Map.singleton oref txOut
-
-      oref = error ()
+      oref = TxOutRef refId 2
 
       txOut =
         PublicKeyChainIndexTxOut
           (pubKeyHashAddress fromPkh)
           (Ada.adaValueOf 10)
 
-  txId <- getCardanoTxId <$> submitTxConstraintsWith @Void ls cs
+  txId <-
+    getCardanoTxId
+      <$> submitTxConstraintsWith @Void lookups constraints
   logInfo @Hask.String $ "Awaiting tx completion: " <> Hask.show txId
   void $ awaitTxConfirmed txId
   logInfo @Hask.String "Tx confirmed, payment complete"
+  where
+    refId :: TxId
+    refId =
+      unsafeDecode
+        "TxId"
+        "{\"getTxId\" : \"206e4a7e1b8a8004d41546ae28089cc4c00853d4f285c45ca62ba8a81b271f41\"}"
