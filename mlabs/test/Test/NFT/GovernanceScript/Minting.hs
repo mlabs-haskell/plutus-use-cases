5e285495101529dee8800d1e5fc68c96b6227ea6
Kirill Boltaev <aske@fmap.me>
2021-12-07 18:57:44 +0300

    Fix validMint context after validator update

--- mlabs/test/Test/NFT/GovernanceScript/Minting.hs
+++ mlabs/test/Test/NFT/GovernanceScript/Minting.hs
@@ -2,18 +2,18 @@ module Test.NFT.GovernanceScript.Minting (
   testMinting,
 ) where
 
+import Data.Maybe (fromJust)
 import Data.Semigroup ((<>))
 import Ledger qualified
-import Mlabs.NFT.Types (UserId(..))
 import Mlabs.NFT.Governance.Types qualified as NFT
 import Mlabs.NFT.Governance.Validation qualified as NFT
+import Mlabs.NFT.Types (UserId (..))
 import PlutusTx qualified
 import PlutusTx.Prelude hiding ((<>))
 import Test.NFT.GovernanceScript.Values as TestValues
 import Test.Tasty (TestTree, localOption)
 import Test.Tasty.Plutus.Context
 import Test.Tasty.Plutus.Script.Unit
-import Data.Maybe (fromJust)
 
 govMintValidatorHash :: Ledger.ValidatorHash
 govMintValidatorHash = fromJust $ Ledger.toValidatorHash TestValues.govScriptAddress
@@ -21,14 +21,14 @@ govMintValidatorHash = fromJust $ Ledger.toValidatorHash TestValues.govScriptAdd
 testMinting :: TestTree
 testMinting =
   localOption (TestCurrencySymbol TestValues.nftCurrencySymbol) $
-  localOption (TestValidatorHash govMintValidatorHash) $
-    withMintingPolicy "Test NFT-Gov minting policy" nftGovMintPolicy $ do
-      shouldValidate "Valid init" validInitData validInitCtx
-      shouldn'tValidate "Init: missing list head" validInitData initMissingHeadCtx
-      shouldn'tValidate "Init: not minting proof token" validInitData initNoProofTokenCtx
-       -- || (nodeCanBeInserted && nodeInsertedWithCorrectValues))
-      shouldValidate "Valid mint gov (first new node case)" validMintData validMintCtx
-      shouldn'tValidate "Can't mint with Proof redeemer" proofRedeemerData proofRedeemerCtx
+    localOption (TestValidatorHash govMintValidatorHash) $
+      withMintingPolicy "Test NFT-Gov minting policy" nftGovMintPolicy $ do
+        shouldValidate "Valid init" validInitData validInitCtx
+        shouldn'tValidate "Init: missing list head" validInitData initMissingHeadCtx
+        shouldn'tValidate "Init: not minting proof token" validInitData initNoProofTokenCtx
+        -- (nodeCanBeInserted && nodeInsertedWithCorrectValues))
+        shouldValidate "Valid mint gov (first new node case)" validMintData validMintCtx
+        shouldn'tValidate "Can't mint with Proof redeemer" proofRedeemerData proofRedeemerCtx
 
 testGovHead :: NFT.GovLList
 testGovHead = NFT.HeadLList (NFT.GovLHead (1 % 100) "") Nothing
@@ -36,9 +36,9 @@ testGovHead = NFT.HeadLList (NFT.GovLHead (1 % 100) "") Nothing
 validInitCtx :: ContextBuilder 'ForMinting
 validInitCtx =
   -- mintsWithSelf TestValues.testTokenName 1
-    paysSelf TestValues.uniqueAndProofTokens (NFT.GovDatum testGovHead)
+  paysSelf TestValues.uniqueAndProofTokens (NFT.GovDatum testGovHead)
     -- list head with unique token
-    <> (input $ Input (OwnType (PlutusTx.toBuiltinData (NFT.GovDatum testGovHead))) TestValues.oneUniqueToken)
+    <> spendsFromOther govMintValidatorHash TestValues.oneUniqueToken (NFT.GovDatum testGovHead)
 
 initMissingHeadCtx :: ContextBuilder 'ForMinting
 initMissingHeadCtx =
@@ -46,9 +46,9 @@ initMissingHeadCtx =
 
 initNoProofTokenCtx :: ContextBuilder 'ForMinting
 initNoProofTokenCtx =
-    paysSelf TestValues.oneUniqueToken (NFT.GovDatum testGovHead)
+  paysSelf TestValues.oneUniqueToken (NFT.GovDatum testGovHead)
     -- list head with unique token
-    <> (input $ Input (OwnType (PlutusTx.toBuiltinData (NFT.GovDatum testGovHead))) TestValues.oneUniqueToken)
+    <> spendsFromOther govMintValidatorHash TestValues.oneUniqueToken (NFT.GovDatum testGovHead)
 
 validInitData :: TestData 'ForMinting
 validInitData = MintingTest NFT.InitialiseGov
@@ -63,12 +63,14 @@ validMintCtx :: ContextBuilder 'ForMinting
 validMintCtx =
   -- new head
   paysSelf TestValues.uniqueAndProofTokensPlus1Ada (NFT.GovDatum testGovNewHead)
-  -- old head
-  -- <> (input $ Input (OwnType (PlutusTx.toBuiltinData (NFT.GovDatum testGovHead))) TestValues.uniqueAndProofTokens)
-  <> spendsFromOther govMintValidatorHash TestValues.uniqueAndProofTokens (NFT.GovDatum testGovHead)
-  -- inserting the first node
-  <> (output $ Output (OwnType (PlutusTx.toBuiltinData (NFT.GovDatum testGovNode))) TestValues.listGovTokens)
-  <> (output $ Output (PubKeyType TestValues.userOnePkh) TestValues.freeGovTokens)
+    -- old head
+    <> spendsFromOther govMintValidatorHash TestValues.uniqueAndProofTokens (NFT.GovDatum testGovHead)
+    -- inserting the first node
+    <> paysOther govMintValidatorHash TestValues.listGovTokens (NFT.GovDatum testGovNode)
+    -- freeGov tokens paid to user
+    <> paysToPubKey TestValues.userOnePkh TestValues.freeGovTokens
+    -- fee
+    <> paysToPubKey "" TestValues.oneAda
 
 validMintData :: TestData 'ForMinting
 validMintData = MintingTest NFT.MintGov
