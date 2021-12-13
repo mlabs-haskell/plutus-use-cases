54a8a6c600a29522271124c4ec482c05e99555bf
Mikhail Lazarev <mikhail@mlabs.city>
2021-12-13 14:54:14 +0300

    WIP: NFT-buy-set-price: - PAB endpoints

--- mlabs/nft-buy-set-price/Main.hs
+++ mlabs/nft-buy-set-price/Main.hs
@@ -1,7 +1,8 @@
 module Main (main) where
 
 import Prelude
+import Mlabs.NFTBuySetPrice.PAB (runBuySetMarketplace)
 
 
 main :: IO ()
-main = print "TDB"
+main = runBuySetMarketplace
--- mlabs/src/Mlabs/NFTBuySetPrice/PAB.hs
+++ mlabs/src/Mlabs/NFTBuySetPrice/PAB.hs
@@ -1,14 +1,68 @@
 module Mlabs.NFTBuySetPrice.PAB (
+  BuySetContract,
   runBuySetMarketplace,
 ) where
 
+import PlutusTx.Prelude
+import Prelude qualified as Hask
+
+import Data.Aeson (
+  FromJSON,
+  ToJSON,
+ )
+import Data.OpenApi.Schema qualified as OpenApi
+
+import GHC.Generics (Generic)
+
+import Prettyprinter (Pretty (..), viaShow)
+
+import Language.PureScript.Bridge (argonaut, equal, genericShow, mkSumType)
+
+import Plutus.PAB.Effects.Contract.Builtin (HasDefinitions (..), SomeBuiltin (..))
+import Plutus.PAB.Effects.Contract.Builtin qualified as Builtin
+import Plutus.PAB.Run.PSGenerator (HasPSTypes (..))
+
+import Mlabs.NFTBuySetPrice.Api
+import Mlabs.NFT.Contract.Init (uniqueTokenName)
+import Mlabs.NFT.Types (UniqueToken)
+
+import Plutus.Contracts.Currency ()
+import Plutus.V1.Ledger.Value (CurrencySymbol (..), TokenName (..), assetClass)
+
 import Plutus.PAB.Effects.Contract.Builtin qualified as Builtin
 import Plutus.PAB.Run (runWith)
-import Prelude
 
-
--- There will be just one ADT to hook it to PAB from what I understand now, probably can just go here
 
 -- | Start PAB for NFT contract
-runBuySetMarketplace :: IO ()
-runBuySetMarketplace = undefined
+runBuySetMarketplace :: Hask.IO ()
+runBuySetMarketplace = runWith (Builtin.handleBuiltin @BuySetContract)
+
+
+{- | Contracts available through PAB.
+ For concrete endpoints see `getContract`
+-}
+data BuySetContract
+  = BuySetContract UniqueToken
+  deriving stock (Hask.Eq, Hask.Ord, Hask.Show, Generic)
+  deriving anyclass (FromJSON, ToJSON, OpenApi.ToSchema)
+
+instance Pretty BuySetContract where
+  pretty = viaShow
+
+instance HasPSTypes BuySetContract where
+  psTypes =
+    [ equal . genericShow . argonaut $ mkSumType @BuySetContract
+    ]
+
+instance HasDefinitions BuySetContract where
+  getDefinitions =
+    [ BuySetContract exampleUT
+    ]
+    where
+      exampleUT = assetClass (CurrencySymbol "ff") (TokenName uniqueTokenName)
+
+  getContract = \case
+    BuySetContract uT -> SomeBuiltin $ buySetEndpoints uT
+
+  getSchema = \case
+    BuySetContract _ -> Builtin.endpointsToSchemas @NFTBySetPriceSchema
