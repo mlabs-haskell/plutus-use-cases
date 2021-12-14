80c6bc5522411bfd1e0280bc53c21ed2d0ebcb30
Mikhail Lazarev <mikhail@mlabs.city>
2021-12-14 18:02:00 +0300

    script to run initialization

---
+++ mlabs/nft-marketplace/run_init.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+WALLET_ID=2d4cc31a4b3116ab86bfe529d30d9c362acd0b44
+WALLET_PKH=9ed7d88109a20fc0ffe82926040f0768a6bd1dbeceb8124a9050ff85
+
+echo "Activating contract"
+contract_id=$(curl -X POST 'localhost:9080/api/contract/activate' \
+                   -H 'Content-Type: application/json' \
+                   -d "{
+                       \"caID\": {
+                           \"tag\": \"NftAdminContract\"
+                       },
+                       \"caWallet\": {
+                           \"getWalletId\": \"$WALLET_ID\"
+                       }
+                   }" \
+              | jq -r '.unContractInstanceId')
+
+echo "NftAdminContract Contract ID: " $contract_id
+sleep 2
+
+echo "Callig \"app-init\" edpoint"
+curl -H 'Content-Type: application/json'  -X POST \
+--data-raw "{
+    \"ip'admins\": [
+        {
+            \"getUserId\": {
+                \"getPubKeyHash\": \"$WALLET_PKH\"
+            }
+        }
+    ],
+    \"ip'feeRate\": [1,2],
+    \"ip'feePkh\": {
+        \"getPubKeyHash\": \"$WALLET_PKH\"
+    }
+}" \
+localhost:9080/api/contract/instance/$contract_id/endpoint/app-init
+# sleep 1
+
+# echo "State:"
+# curl localhost:9080/api/contract/instance/$contract_id/status | jq
