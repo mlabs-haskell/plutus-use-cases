131af4d45b6b2597a494fb492eb9f7989785f613
Mikhail Lazarev <mikhail@mlabs.city>
2021-12-22 13:15:23 +0300

    WIP: December demo - setting price with MlabsPAB

--- mlabs/nft-marketplace-dec-demo/Main.hs
+++ mlabs/nft-marketplace-dec-demo/Main.hs
@@ -35,6 +35,7 @@ import Mlabs.NFT.Types (
 import Mlabs.Plutus.Contracts.CurrencyMP qualified as Contract.Currency
 import Mlabs.NFT.Contract.InitMP qualified as Contract.Init
 import Mlabs.NFT.Contract.MintMP qualified as Contract.Mint
+import Mlabs.NFT.Contract.SetPriceMP qualified as Contract.SetPrice
 import Mlabs.NFT.Contract.Aux (hashData)
 
 import Prelude
@@ -45,42 +46,75 @@ import PlutusTx.Builtins.Class (stringToBuiltinByteString)
 
 import Cardano.Api.Shelley (ProtocolParameters)
 
-data MintHeadParams = MintHeadParams
-  { mhp'uniqueToken :: UniqueToken
-  , mhp'initParams :: InitParams
+-- MINT HEAD
+data MintHeadReq = MintHeadReq
+  { mhr'uniqueToken :: UniqueToken
+  , mhr'initParams :: InitParams
   } deriving stock (Show, Generic)
     deriving anyclass (FromJSON, ToJSON)
 
-data MintData = MintData
-  { mdContent :: String
-  , mdTitle :: String
-  , mdShare :: PP.Rational
-  , mdPrice :: Maybe Integer
+-- MINT NFT
+
+-- Data to substitute original contracts parameter, accepts `Content` as plain text
+data MintNftData = MintNftData
+  { mnd'content :: String
+  , mnd'title :: String
+  , mnd'share :: PP.Rational
+  , mnd'price :: Maybe Integer
   } deriving stock (Show, Generic)
     deriving anyclass (FromJSON, ToJSON)
 
-toMintParams :: MintData -> MintParams
-toMintParams (MintData content title share price) = 
+data MintNftReq = MintNftReq
+  { mnr'uniqueToken :: UniqueToken
+  , mnr'data :: MintNftData
+  } deriving stock (Show, Generic)
+    deriving anyclass (FromJSON, ToJSON)
+
+toMintParams :: MintNftData -> MintParams
+toMintParams (MintNftData content title share price) = 
   MintParams 
-    (Content $ stringToBuiltinByteString  content) 
-    (Title $ stringToBuiltinByteString  title) 
+    (toContent content) 
+    (toTitle title) 
     share 
     price
 
+toContent :: String -> Content
+toContent = Content . stringToBuiltinByteString
 
-data MintNftParams = MintNftParams
-  { mnp'uniqueToken :: UniqueToken,
-    mnp'mintParams :: MintData 
-    -- ^ `MintParams` not used straight to make request more readable for demo
+toTitle :: String -> Title
+toTitle = Title . stringToBuiltinByteString
+
+-- SET NFT PRICE
+
+-- Data to substitute original contracts parameter, accepts `Content` as plain text
+data SetPriceData = SetPriceData
+  { spd'content :: String
+  , spd'price :: Maybe Integer
   } deriving stock (Show, Generic)
     deriving anyclass (FromJSON, ToJSON)
 
+nidFromContent :: Content -> NftId
+nidFromContent = NftId . hashData
+
+toSetPriceParams :: SetPriceData -> SetPriceParams
+toSetPriceParams (SetPriceData content price) =
+  SetPriceParams 
+    (nidFromContent $ toContent content) 
+    price
+
+data SetPriceReq = SetPriceReq
+  { spr'uniqueToken :: UniqueToken
+  , spr'data :: SetPriceData
+  } deriving stock (Show, Generic)
+    deriving anyclass (FromJSON, ToJSON)
+
+-- PAB CONTRACTS
 data NftDemoContracts
   = GenerateUniqueToken
-  | MintListHead MintHeadParams
-  | MintNft MintNftParams
+  | MintListHead MintHeadReq
+  | MintNft MintNftReq
+  | SetNftPice SetPriceReq
   -- | BuyNFT -- TODO
-  -- | SetNftPice -- TODO
   deriving stock (Show, Generic)
   deriving anyclass (FromJSON, ToJSON)
 
@@ -91,14 +125,17 @@ instance HasDefinitions NftDemoContracts where
     GenerateUniqueToken -> endpointsToSchemas @Contract.Currency.CurrencySchema
     MintListHead _ -> endpointsToSchemas @EmptySchema
     MintNft _ -> endpointsToSchemas @EmptySchema
+    SetNftPice _ -> endpointsToSchemas @EmptySchema
 
   getContract = \case
     GenerateUniqueToken 
       -> SomeBuiltin $ Contract.Currency.mintCurrency Contract.Init.uniqueTokenName
-    MintListHead (MintHeadParams ut initPs) 
+    MintListHead (MintHeadReq ut initPs) 
       -> SomeBuiltin $ Contract.Init.initApp ut initPs
-    MintNft (MintNftParams ut mintData) 
+    MintNft (MintNftReq ut mintData) 
       -> SomeBuiltin $ Contract.Mint.mint ut (toMintParams mintData)
+    SetNftPice (SetPriceReq ut setPriceData)
+      -> SomeBuiltin $ Contract.SetPrice.setPrice ut (toSetPriceParams setPriceData)
 
 
 
@@ -123,8 +160,8 @@ main = do
   MLabsPAB.runPAB @NftDemoContracts pabConf
   where
     logExampleParams mps = do
-      let nftId = NftId . hashData . mp'content $ mps
-          setPricePs = SetPriceParams nftId (Just 33)
+      let nftId = nidFromContent . mp'content $ mps
+          setPricePs = SetPriceData "Content" (Just (-33))
       LBS.putStrLn ("Mint parameters: " <> JSON.encode mps)
       LBS.putStrLn ("Content hash: " <> JSON.encode nftId)
       LBS.putStrLn ("Set price: " <> JSON.encode setPricePs)
---
+++ mlabs/src/Mlabs/NFT/Contract/SetPriceMP.hs
@@ -0,0 +1,84 @@
+{-# LANGUAGE UndecidableInstances #-}
+
+module Mlabs.NFT.Contract.SetPriceMP (
+  setPrice,
+) where
+
+import PlutusTx.Prelude hiding (mconcat, mempty, (<>))
+import Prelude (mconcat)
+import Prelude qualified as Hask
+
+import Control.Lens ((^.))
+import Control.Monad (void, when)
+
+import Data.Map qualified as Map
+import Data.Monoid (Last (..))
+import Data.Text (Text)
+
+--import Mlabs.Plutus.Contract ()
+import Plutus.Contract (Contract)
+import Plutus.Contract qualified as Contract
+import PlutusTx qualified
+
+import Ledger (
+  Redeemer (..),
+  ciTxOutValue,
+ )
+
+import Ledger.Constraints qualified as Constraints
+import Ledger.Typed.Scripts (validatorScript)
+
+--import Ledger.Value as Value (AssetClass (..), TokenName (..), singleton)
+--import Plutus.ChainIndex.Tx ()
+
+import Mlabs.NFT.Contract.Aux
+import Mlabs.NFT.Types
+import Mlabs.NFT.Validation
+import Mlabs.Utils
+
+{- |
+  Attempts to set price of NFT, checks if price is being set by the owner
+  and that NFT is not on an auction.
+-}
+setPrice :: UniqueToken -> SetPriceParams -> Contract UserWriter Contract.EmptySchema Text ()
+setPrice ut SetPriceParams {..} = do
+  aSymbol <- getNftAppSymbol ut
+  when negativePrice $ Contract.throwError "New price can not be negative"
+  ownOrefTxOut <- getUserAddr >>= fstUtxoAt
+  ownPkh <- Contract.ownPubKeyHash
+  PointInfo {..} <- findNft sp'nftId ut
+  oldNode <- case pi'data of
+    NodeDatum n -> Hask.pure n
+    _ -> Contract.throwError "NFT not found"
+  when (getUserId ((info'owner . node'information) oldNode) /= ownPkh) $
+    Contract.throwError "Only owner can set price"
+  when (isJust . info'auctionState . node'information $ oldNode) $
+    Contract.throwError "Can't set price auction is already in progress"
+
+  let nftDatum = NodeDatum $ updateDatum oldNode
+      nftVal = pi'CITxO ^. ciTxOutValue
+      action = SetPriceAct sp'price aSymbol
+      lookups =
+        mconcat
+          [ Constraints.unspentOutputs $ Map.fromList [ownOrefTxOut]
+          , Constraints.unspentOutputs $ Map.fromList [(pi'TOR, pi'CITxO)]
+          , Constraints.typedValidatorLookups (txPolicy ut)
+          , Constraints.otherScript (validatorScript . txPolicy $ ut)
+          ]
+      tx =
+        mconcat
+          [ Constraints.mustPayToTheScript nftDatum nftVal
+          , Constraints.mustSpendPubKeyOutput (fst ownOrefTxOut)
+          , Constraints.mustSpendScriptOutput
+              pi'TOR
+              (Redeemer . PlutusTx.toBuiltinData $ action)
+          ]
+
+  void $ submitTxConstraintsWithUnbalanced @NftTrade lookups tx
+  Contract.logInfo @Hask.String "set-price successful!"
+  where
+    updateDatum node = node {node'information = (node'information node) {info'price = sp'price}}
+
+    negativePrice = case sp'price of
+      Nothing -> False
+      Just v -> v < 0
